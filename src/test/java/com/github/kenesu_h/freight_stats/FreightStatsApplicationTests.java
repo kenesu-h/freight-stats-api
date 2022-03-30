77c80cd59282f811099172aa365bd32617bce3d0
Kenneth <8515878+kenesu-h@users.noreply.github.com>
2022-03-30 17:47:12 -0400

    Copied over all files from original repo.

---
+++ src/test/java/com/github/kenesu_h/freight_stats/FreightStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kenesu_h.freight_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
