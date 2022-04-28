a75bf6c60975926e91906aa9ee21b5f07177dd69
Kenneth <8515878+kenesu-h@users.noreply.github.com>
2022-04-27 22:16:05 -0400

    Fixed one of the SQL scripts.

--- mysql/load_covid.sql
+++ mysql/load_covid.sql
@@ -2,6 +2,6 @@ use cs3200Project;
 
 SET GLOBAL local_infile = true;
 truncate covid_data;
-LOAD DATA LOCAL INFILE 'ABSOLUTE/PATH/TO/SHIPMENT/shipment.csv' INTO TABLE covid_data
+LOAD DATA LOCAL INFILE 'ABSOLUTE/PATH/TO/SHIPMENT/covid_data.csv' INTO TABLE covid_data
 -- Place the path to your file: /Users/__YOUR_USERNAME_/Downloads/covid_data.csv on macos
 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
